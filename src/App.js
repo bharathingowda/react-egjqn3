a8b6d577b08b305f6851d65d2c0359e698dbbe78
bharathingowda <bharathigowda20@gmail.com>
2021-08-26 11:44:22 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ class ClassComponent extends React.Component {
   render() {
     return (
       <div style={{ margin: '50px' }}>
-        <h1>Welcome to Geeks for Geeks </h1>
+        <h1>Welcome</h1>
         <h3>Counter App using Class Component : </h3>
         <h2> {this.state.count}</h2>
         <button onClick={this.increase}> Add</button>
